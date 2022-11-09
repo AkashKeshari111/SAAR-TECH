a3124c03943ce96ee0a1cbca3fb25208114ed4db
anjanak05 <anjna4523@gmail.com>
2022-11-10 03:06:04 +0530

    login and sign up routes added

---
+++ frontend/src/Pages/Home/UserLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserLogin = () => {
+  return (
+    <div>UserLogin</div>
+  )
+}
+
+export default UserLogin
--- frontend/src/Router/AllRoutes.jsx
+++ frontend/src/Router/AllRoutes.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from '../Pages/Home/HomePage'
+import AdminLogin from "../Pages/Home/AdminLogin"
+import UserLogin from "../Pages/Home/UserLogin.jsx"
+import AdminRegister from "../Pages/Home/AdminRegister.jsx"
+import UserRegister from "../Pages/Home/UserRegister.jsx"
+
+
 
 const AllRoutes = () => {
   return (
   <Routes>
     <Route path="/" element={<HomePage/>}/>
+    <Route path="/AdminLogin" element={<AdminLogin/>}/>
+    <Route path="/UserLogin" element={<UserLogin/>}/>
+    <Route path="/AdminRegister" element={<AdminRegister/>}/>
+    <Route path="/UserRegister" element={<UserRegister/>}/>
   </Routes>
   )
 }
