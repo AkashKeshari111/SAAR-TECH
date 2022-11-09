a15310db03df37f339dddefe7c33f1a95bebf9da
anjanak05 <anjna4523@gmail.com>
2022-11-10 02:47:41 +0530

    navbar footer done

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React from "react";
+import styles from "../Navbar/Navbar.module.css";
+import { BsChevronDown } from "react-icons/bs";
+import { IoIosPhonePortrait } from "react-icons/io";
+import { HiOutlineUser, HiOutlineHeart } from "react-icons/hi";
+import { FiShoppingCart } from "react-icons/fi";
+import { BiSearch } from "react-icons/bi";
+import Signinfunction from "./Signinfunction.jsx";
 
 const Navbar = () => {
+   const menuItems = [
+    {
+      title: 'Consumer Electronics',
+      url: '/consumer_Electronics',
+    },
+    {
+      title: 'Computers, Tablets & Office',
+      url: '/Computers',
+    },
+    {
+      title: 'Health & Personal Care',
+      url: '/Health',
+    },
+    {
+      title:"Outdoors, Fitness & Sports",
+      url:"/Outdoors"
+    },
+    {
+      title: 'Motor & Car Electronics',
+      url: '/Motors',
+    },
+    {
+      title: 'Health & Personal Care',
+      url: '/Health',
+    },
+    {
+      title:"Watches & Jewelry",
+      url:"/Watches"
+    }
+  ];
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className={styles.outerbar}>
+      <div className={styles.containerNavbar}>
+        {/* <a href="" alt=""></a> */}
+        <div className={styles.topNavbarSEction}>
+          <img src="footerLogo.png" alt=""></img>
+          <div>
+            <div className={styles.navbarlnguageSection}>
+              <p>
+                {" "}
+                <IoIosPhonePortrait className={styles.icksPhone} /> Save $3 with
+                App <BsChevronDown className={styles.icks} />{" "}
+              </p>
+              <p>
+                {" "}
+                Support Center <BsChevronDown className={styles.icks} />{" "}
+              </p>
+              <p>
+                {" "}
+                Language <BsChevronDown className={styles.icks} />{" "}
+              </p>
+              <p>
+                {" "}
+                Country Website <BsChevronDown className={styles.icks} />{" "}
+              </p>
+            </div>
+            <div className={styles.navbarSearchSection}>
+              <div>
+                <select>
+                  <option>All</option>
+                </select>
+                <input placeholder="Search Products" />
+                <p>
+                  <BiSearch size={"23px"}></BiSearch>
+                </p>
+              </div>
+              <p><Signinfunction></Signinfunction></p>
 
-export default Navbar
+              <a href="#" alt="">
+                <HiOutlineHeart size={"25px"}></HiOutlineHeart>
+                <p>Favourites</p>
+              </a>
+
+
+              <a href="#" alt="">
+                <FiShoppingCart size={"25px"} color="#ffda00"></FiShoppingCart>
+                <p>Cart</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.mainDropdown}>
+        {menuItems.map((e)=>(
+          <p>{e.title}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
