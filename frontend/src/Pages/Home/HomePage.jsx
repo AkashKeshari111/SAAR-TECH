382c5252699af285ac8834a050093a28f35762c3
monga0066 <106160353+monga0066@users.noreply.github.com>
2022-11-09 21:47:16 +0530

    rahul

--- frontend/src/Pages/Home/HomePage.jsx
+++ frontend/src/Pages/Home/HomePage.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import './Homepage.css'
+import Card from './card'
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='main-home-container'>
+      {/* firs images start */}
+      <div className='f-div'>
+        <div>
+          <img src="https://uidesign.gbtcdn.com/GB/image/6874/230x120b_en.jpg" alt="" />
+        </div>
+        <div>
+          <img src="https://uidesign.gbtcdn.com/GB/image/8823/T14_230X120_en.jpg" alt="" />
+        </div>
+        <div>
+          <img src="https://uidesign.gbtcdn.com/GB/image/7257/230_120_en.jpg" alt="" />
+        </div>
+        <div>
+          <img src="https://uidesign.gbtcdn.com/GB/image/6874/230x120b_en.jpg" alt="" />
+        </div>
+        <div>
+          <img src="https://uidesign.gbtcdn.com/GB/image/6080/230x120.jpg?imbypass=true" alt="" />
+        </div>
+        </div>
+       {/* firs images end */}
+          
+      {/* items button  start*/}
+      <div className='items-1'>
+      <Card/>
+      </div>
+      
+
+    {/* items button end */}
+    </div>
   )
 }
 
---
+++ frontend/src/Pages/Home/card.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Homepage.css'
+export default function Card() {
+
+
+
+  return (
+    <div className='card'>
+      <div className='collection'>
+
+        <h2>
+          
+        </h2>
+
+      </div>
+      <div>
+    <img src="https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16654/goods_thumb_220-v3/b14e634a2e36.jpg" alt="" />
+    </div>
+    <div className='card-p'>
+     <p>Nimble Tech Store</p>
+     </div>
+     <button className='add-cart'>$24</button>
+
+    </div>
+  )
+}
