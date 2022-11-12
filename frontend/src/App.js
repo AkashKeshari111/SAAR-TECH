acc630853e09e0ac7c18e7987f6188c84c7567d3
anjanak05 <anjna4523@gmail.com>
2022-11-12 12:09:32 +0530

    user registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Singleproduct from './Pages/single/singleproduct';
 import MensFasion from './Pages/product/MensFeshion';
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 import AllRoutes from "./Router/AllRoutes";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,9 +13,9 @@ function App() {
     <div className="App">
 
 {/* <------------commnet---------------> */}
- 
+      <Navbar />
       <AllRoutes />
-
+      <Footer />
     {/* <MensFasion/> */}
 
     </div>
